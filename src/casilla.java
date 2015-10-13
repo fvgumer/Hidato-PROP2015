b2569acf18d78b6141958ad3249c2943d7f49987
AlexCatala <alex.catala.cabello@gmail.com>
2015-10-13 15:07:17 +0200

    Primeres classes i algoritmes

---
+++ src/casilla.java
@@ -0,0 +1,17 @@
+
+public class casilla {
+
+	private int value;
+	
+	public casilla() {
+		value=-1;
+	}
+	
+	public void setvalue(int n) {
+		value = n;
+	}
+	
+	public int getvalue() {
+		return value;
+	}
+}
