fb56537ad80aafb68256063c0033ac69fb3f6ba9
Elena <elena.ruiz.bdn@gmail.com>
2015-10-16 20:04:37 +0200

    He creado una nueva clase para probar hacer tableros aleatorios, no esta acabado de implementar i no es nada random jajajajja

--- src/Partida.java
+++ src/Partida.java
@@ -6,11 +6,37 @@
 
 public class Partida {
 	//Tablero tablero1;
+	int id_partida;
+	
+	int estado; 
+	public static int READY = 1;
+	public static int PAUSE = 2; //Codificación de estado
+	
 	int dificultad;
+	public static int NOTIENE = 0;
 	public static int ALTO = 1;
 	public static int MEDIO = 2;
-	public static int BAJO = 3;
+	public static int BAJO = 3; //Codificación de dificultad
+	
 	Reloj R1;
+	
+	int modo;
+	public static int CONTRA = 1;
+	public static int CLASIC = 2;
+	public static int EXTREM = 3; //Codificación modo
+	
+	public Partida(){
+		//this.id_partida <- Se ha de generar a partir de fichero
+		this.estado = 2;
+		this.dificultad = NOTIENE;
+		this.modo = NOTIENE;
+	}
+	
+	public void set_partida(int dificultad, int modo){
+		this.estado = 2;
+		this.dificultad = dificultad;
+		this.modo = modo;
+	}
 	
 	
 
