e1d759b731ae189da091b9e0ee6843d736aa1e45
Elena <elena.ruiz.bdn@gmail.com>
2015-10-17 17:29:17 +0200

    Posicions no alearories

--- src/Crear_tablero.java
+++ src/Crear_tablero.java
@@ -35,16 +35,16 @@ public class Crear_tablero {
 		int x = 0;
 		int y = 0;
 		for (posats = 0; posats < forats; ++posats){
-			x =  (rm.nextInt()*posats)-forats+rm.nextInt();
+			x =  (rm.nextInt()*posats)-forats+rm.nextInt(); //Calcula una "x" ALEATORIA
 			x = x%this.n;
 			if (x < 0) x = x*(-1);
-			y =  (rm.nextInt()+forats)-posats*rm.nextInt();
+			y =  (rm.nextInt()+forats)-posats*rm.nextInt(); //Calcula un "y" ALEATORIA
 			y = y%this.n;
 			if (y < 0) y = y*(-1);
 			System.out.println(x);
 			System.out.println(y);
 			int contador = 0;
-			while (this.Matrix[x][y] == -1) { //Busca posició no buida
+			while (this.Matrix[x][y] == -1) { //Sigue hasta que encuentra posición vacía
 				if (contador < this.n) {
 					y = (y+1)%this.n;
 					++contador;
@@ -58,6 +58,10 @@ public class Crear_tablero {
 		}
 	}
 	
+	//IMPRIME TABLERO
+	//Forats = "X"
+	//Casillas vacías = "."
+	//Casillas iniciales = num
 	public void imprimir_tablero(){
 		for (int i = 0; i < this.n; ++i){
 			for (int j = 0; j < this.n; ++j){
@@ -69,23 +73,54 @@ public class Crear_tablero {
 		}
 	}
 	
+	private boolean posiciones_validas(int x, int y, int n, int[][] Matriu){
+		if ( x < 0 || y < 0 ) return false;
+		else if ( x > n || y > n) return false;
+		else if (Matriu[x][y] == -1) return false;
+		//Falta mirar que no faci illes
+		return true;
+	}
+	
+	public void introducir_forats(int abujeros) {
+		System.out.println("Introduce pares de <x y> en posiciones validas");
+		rm = new Random();
+		int x, y;
+		while(abujeros > 0){
+			System.out.println("Te quedan introducir " + abujeros + " forats.");
+			System.out.println("Introduce >> x y");
+			while(rm.nextInt() null);
+			
+			y = rm.nextInt();
+			if (this.posiciones_validas(x,y,this.n, this.Matrix)) {
+				this.Matrix[x][y] = -1;
+				--abujeros;
+			}
+			else System.out.println("Posiciones no validas, vuelve a introducir");
+		}	
+	}
+	
 	
 	public static void main(String[] args) {
 		sc = new Scanner(System.in);
-		/*Entamos dimensiones*/
+		/*1. Entramos dimensiones*/
 		System.out.println("Escriu les dimensions de la matriu nxn");
 		int dim = sc.nextInt();
 		Crear_tablero TB = new Crear_tablero(dim); //CREAMOS TABLERO
-		/*Entramos abujeros*/
+		
+		/*2. Entramos abujeros*/
 		System.out.println("Vale, la teva matriu es de "+ dim + "x" +
 							dim + "ara escriu els forats que vols\n");
 		int abujeros = sc.nextInt();
+		
+		
 		System.out.println("Ara que sabem que vols "+ abujeros + " forats.\n" +
 				"Els vols distribuír aleatoriament? (POSA 1) \n" +
 				"O vols ficar les seves posicions? (POSA2) \n");
 		
+		/*3. Elegimos modo de poner abujeros*/
 		int modo = sc.nextInt();
-		if (modo == 1)  TB.posar_forats_aleatori(abujeros);
+		if (modo == 1)  TB.posar_forats_aleatori(abujeros); //Forma tablero
+		if (modo == 2)  TB.introducir_forats(abujeros);
 		TB.imprimir_tablero();
 		
 
