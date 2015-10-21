ca22f64c2d0b458b22f9ab24e0f8279f3b93cc5f
belen95 <belen_95_22@hotmail.es>
2015-10-21 16:14:26 +0200

    Update Usuario.java

--- src/Usuario.java
+++ src/Usuario.java
@@ -7,19 +7,6 @@ public class Usuario {
 		//añadir id_usuario a la BD, establecer current user
 	}
 	
-	public void Cargar_usuario(String u){
-		//buscar id del usuario u y cambiar el current user
-	}
-	
-	public void Eliminar_usuario(){
-		//eliminar de la BD: id_usuario, partidas creadas, estadísticas
-		//recalcular rankings
-		//borrar el current user
-	}
-	
-	public void Cerrar_sesion() {
-		//borrar el current_user
-	}
 	
 	public void Editar_usuario(String u) {
 		this.u = u;
