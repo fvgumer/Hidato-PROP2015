eb9802a7d854e80a3fb00506125534171f994023
AlexCatala <alex.catala.cabello@gmail.com>
2015-10-22 21:06:16 +0200

    El solver ja funciona!

---
+++ src/Casilla.java
@@ -0,0 +1,17 @@
+
+public class Casilla {
+
+	private int value;
+	
+	public void casilla(){
+		value = -1;
+	}
+	
+	public void setvalue(int n){
+		value = n;
+	}
+	
+	public int getvalue(){
+		return value;
+	}
+}
