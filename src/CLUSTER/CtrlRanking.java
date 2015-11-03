89929e4e78141ff1e0eb09b59fb75fbdd0cf2f62
belen95 <belen_95_22@hotmail.es>
2015-11-03 18:31:21 +0100

    Create CtrlRanking.java
    no se si estan bien las especificaciones porque hablo de ficheros y eso es estructura interna del programa

---
+++ src/CLUSTER/CtrlRanking.java
@@ -0,0 +1,29 @@
+package CLUSTER;
+
+import java.util.*;
+
+public class CtrlRanking {
+	
+	//ordenar nuevo resultado guardado
+	//pedir n mejores resultados de un tablero
+	
+	/* Pre: j, m, d y p corresponden a los datos relevantes (username jugador, modo, 
+	 * dificultad, puntuación) de la última partida finalizada del tablero con nombre t */
+	public void anadirResultado(String t, String j, String m, String d, int p) {
+		
+	}
+	/* Post: Se han añadido los datos relevantes al fichero de resultados
+	 * del tablero con nombre t, se ha añadido un nuevo índice al fichero de índices de 
+	 * resultados de dicho tablero y este se ha ordenado debidamente */
+	
+	/* Pre: El fichero de índices de resultados del tablero con nombre t está
+	 * ordenado decrecientemente según las puntuaciones correspondientes indicadas
+	 * en el fichero de resultados de dicho tablero */
+	public void obtenerResultados(String t, int n) {
+		
+	}
+	/* Post: Se muestran por pantalla los n mejores resultados del tablero
+	 * con nombre t */
+	
+	
+}
