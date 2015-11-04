414d13e2ec32d8eb32d2fd3cae92fd72cbfc3dd7
fvgumer <joel.codina@est.fib.upc.edu>
2015-11-04 15:38:22 +0100

    clase jugador i gestor
    sad

---
+++ src/CLUSTER/ClassJugador.java
@@ -0,0 +1,29 @@
+import java.io.Serializable;
+
+public class Jugador implements Serializable{
+	
+	private static final long serialVersionUID = 1846695613718020013L;
+	public String nombre;
+	public String password;
+
+Jugador(String nombre, String password){
+this.nombre = nombre;
+this.password = password;
+};
+
+public String consultar_nombre(){
+return this.nombre;	
+}
+
+public String consultar_password(){
+return this.password;
+}
+
+public void set_password(String newpassword){
+	this.password=newpassword;
+}
+
+public void set_nombre(String newnombre){
+	this.nombre=newnombre;
+}
+}
