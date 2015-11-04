0a558849755656cbbccea2177b33cd1417ed9a2a
Elena_Ruiz <elena.ruiz.bdn@gmail.com>
2015-11-04 10:45:54 +0100

    Revert "Merge remote-tracking branch 'origin/master'"
    This reverts commit 9b68a3e31833e0372a3fae5cd7b9aa8c6821b765.

---
+++ src/G45/Partida_Comp.java
@@ -0,0 +1,56 @@
+<<<<<<< HEAD
+package G45;
+import CLUSTER.tablero;
+//import CLUSTER.Usuario;
+=======
+import CLUSTER.tablero;
+
+>>>>>>> origin/master
+
+public class Partida_Comp {
+
+	protected static tablero T;
+	private Usuario U;
+	private int ID;
+	
+	public Partida_Comp() {
+		
+	}
+	
+	public void anadir_carc_PC(tablero T, Usuario U, int ID){
+		Partida_Comp.T = new tablero(0);
+		Partida_Comp.T = T;
+		this.ID = ID;
+		Partida_Comp.U  = new Usuario();
+		Partida_Comp.U = U;
+		
+	}
+	
+	public static void ayudar(){
+		
+	}
+	
+	
+	public void modificar_casilla(int i, int j, int valor){
+<<<<<<< HEAD
+		T.setcell(i,j,valor);
+		
+=======
+		T.setcell(j,i,valor);
+>>>>>>> origin/master
+	}
+	
+	public static tablero mostrar_tablero(){
+		
+		return T;
+	}
+	
+	public static void guardar_partida(){
+		
+	}
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
