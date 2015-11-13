0f270a337f6135b5c5426586f15e750afd8f172f
belen95 <belen_95_22@hotmail.es>
2015-11-13 17:29:54 +0100

    Update and rename Ranking.java to ClassRanking.java

---
+++ src/CLUSTER/ClassRanking.java
@@ -0,0 +1,25 @@
+package CLUSTER;
+
+
+import java.util.*;
+
+public class ClassRanking {
+	
+	private ArrayList<ClassResultado> ranking;
+	
+	ClassRanking() {
+		ranking = new ArrayList<ClassResultado>();	
+	}
+
+	private void setResultado_Ranking() {
+		
+	}
+	
+	/* Pre: */
+	public void getPosicion_Ranking(int n) {
+		int x = ranking.get(n); 
+		//buscar linea x en el fichero de resultados
+		//imprimir resultado obtenido
+	}
+	/* Post: devuelve la posicion n del ranking de una partida */
+}
