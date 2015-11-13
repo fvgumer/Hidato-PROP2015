16a049fe3734abc15a85ff13046f98761fa20d5c
belen95 <belen_95_22@hotmail.es>
2015-11-13 21:50:02 +0100

    Update ClassRanking.java

--- src/BELEN/ClassRanking.java
+++ src/BELEN/ClassRanking.java
@@ -2,24 +2,33 @@ package BELEN;
 
 
 import java.util.*;
+import java.io.Serializable;
 
-public class ClassRanking {
+public class ClassRanking implements Serializable {
 	
 	private ArrayList<ClassResultado> ranking;
+	
+	private static final long serialVersionUID = 2L;
 	
 	ClassRanking() {
 		ranking = new ArrayList<ClassResultado>();	
 	}
-
-	private void setResultado_Ranking() {
-		
+	
+	public int size() {
+		return ranking.size();
+	}
+	
+	public void anadirResultado(int pos, ClassResultado res) {
+		ranking.add(pos,res);
+	}
+	
+	public ClassResultado getPosicion(int pos){
+		return ranking.get(pos);
 	}
 	
 	/* Pre: */
-	public void getPosicion_Ranking(int n) {
-		int x = ranking.get(n); 
-		//buscar linea x en el fichero de resultados
-		//imprimir resultado obtenido
+	public void mostrarPosicion(int pos) {
+		ranking.get(pos).mostrarResultado();
 	}
 	/* Post: devuelve la posicion n del ranking de una partida */
 }
