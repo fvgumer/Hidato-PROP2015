6a67c3b6fdcc551796b686e88ed3e6611e5d132f
belen95 <belen_95_22@hotmail.es>
2015-11-14 00:06:51 +0100

    Update ClassEstadisticas.java

--- src/BELEN/ClassEstadisticas.java
+++ src/BELEN/ClassEstadisticas.java
@@ -5,6 +5,8 @@ import java.io.Serializable;
 
 public class ClassEstadisticas implements Serializable{
 
+	private String user;
+	
 	private int partidasJugadas;
 	private int segundosJugados;	//para mostrarlo por pantalla lo pasaremos a hh:mm:ss
 	private int puntuacionTotal;
@@ -17,6 +19,10 @@ public class ClassEstadisticas implements Serializable{
 	ClassEstadisticas(){
 		partidasJugadas = segundosJugados = puntuacionTotal = mejorPuntuacion = 0;
 		tablerosCreados = new ArrayList<String>();
+	}
+
+	public String getName() {
+		return user;
 	}
 	
 	/*Pre: */
