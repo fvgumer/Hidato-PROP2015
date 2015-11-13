8a39d9e57d54aac892255e156c4cf6dac4de08ee
belen95 <belen_95_22@hotmail.es>
2015-11-13 21:59:00 +0100

    Update CtrlRanking.java

--- src/BELEN/CtrlRanking.java
+++ src/BELEN/CtrlRanking.java
@@ -53,6 +53,7 @@ public class CtrlRanking {
 	}
 	
 	public void getTop(String tablero, int n){
+		System.out.print("Jugador  ||  Modo  ||  Dificultad  || Puntuación\n")	//las lineas no se veran alineadas entre si :'(
 		for (int i = 0; i < n; ++i){
 			R.mostrarPosicion(i);
 		}
