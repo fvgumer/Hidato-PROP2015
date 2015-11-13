7cf0be00410ce6227fdad1646a630cc6b062b7a1
belen95 <belen_95_22@hotmail.es>
2015-11-13 17:51:12 +0100

    Create ClassResultado.java

---
+++ src/CLUSTER/ClassResultado.java
@@ -0,0 +1,53 @@
+package CLUSTER;
+
+public class ClassResultado {
+
+	//private String tablero;
+	private String jugador;
+	private String modo;
+	private String dificultad;
+	private int puntuacion;
+	
+	ClassResultado(String jugador, String modo, String dificultad, int puntuacion) {
+		this.jugador = jugador;
+		this.modo = modo;
+		this.dificultad = dificultad;
+		this.puntuacion = puntuacion;
+	}
+	
+	private void setJugador(String j) {
+		jugador = j;
+	}
+	
+	private void setModo(String m) {
+		modo = m;
+	}
+
+	private void setDificultad(String d) {
+		dificultad = d;
+	}
+
+	private void setPuntuacion(int p) {
+		puntuacion = p;
+	}
+	
+	private ClassResultado getResultado(){
+		return this;
+	} 
+	
+	private String getJugador() {
+		return jugador;
+	}
+	
+	private String getModo() {
+		return modo;
+	}
+	
+	private String getDificultad() {
+		return dificultad;
+	}
+	
+	private int getPuntuacion() {
+		return puntuacion;
+	}
+}
