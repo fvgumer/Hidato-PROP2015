846fdb72d43db8195e5dc96c744639b48854a6ab
fvgumer <joel.codina@est.fib.upc.edu>
2015-11-14 15:47:41 +0100

    bug

--- src/JOEL/DriverGestionPartida.java
+++ src/JOEL/DriverGestionPartida.java
@@ -4,18 +4,56 @@ import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.ObjectOutputStream;
+import java.util.Scanner;
 
+import ALEX.Tablero;
 import ELENA.*;
 
 public class DriverGestionPartida {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		ClassPartidaHidato P = new ClassPartidaHidato(null, null, 999, 0);
-		Jugador J = new Jugador("joel3","codina");
+		Scanner scan = new Scanner(System.in);
+		Jugador J = new Jugador("joel","codina");
+		Tablero T = new Tablero(4);
 		CtrlGestionPartida gestor = new CtrlGestionPartida();
-		gestor.guardar(J);
-		
-		
+		int i=0;
+		while(i != -1){
+			System.out.print("Introduce codigo operación  2 = eliminar , 3= guardar, 4=cargar, 5=existeix?");
+			i = scan.nextInt();
+			scan.nextLine(); //Para consumir la /n
+			if (i==2){
+				System.out.println("Introducir id partida que queremos eliminar");
+				int id = scan.nextInt();
+				scan.nextLine();
+				Partida_Hidato P = new Partida_Hidato(T, J, id, 4);
+				gestor.eliminar(P);
+			}
+			else if(i==3){
+				System.out.println("Introducir id partida que queremos guardar");
+				int id = scan.nextInt();
+				scan.nextLine();
+				Partida_Hidato P = new Partida_Hidato(T, J, 342342, 4);
+				System.out.println(P.getUsuario().consultar_nombre());
+				gestor.guardar(P);
+			}
+			else if(i==4){
+				System.out.println("Introducir nombre jugador y id de la partida que queremos cargar");
+				Partida_Hidato x;
+				/*String nombre = scan.nextLine();
+				int id = scan.nextInt();
+				scan.nextLine();*/
+				x = gestor.cargar("joel", 342342);
+			if(x!=null)System.out.println("Partida con id " + x.getUsuario() + " del jugador ") ;//+ x.getUsuario().consultar_nombre() + " cargado");
+			}
+			else if(i==5){
+				System.out.println("Introducir id partida que queremos validar del usuario joel");
+				int id = scan.nextInt();
+				scan.nextLine();
+				Partida_Hidato P = new Partida_Hidato(T, J, id, 4);
+				if(gestor.existeix(P)) System.out.println("Existeix la partida");
+				else System.out.println("No existeix la partida");
+			}
+		}	
 	}
 }
