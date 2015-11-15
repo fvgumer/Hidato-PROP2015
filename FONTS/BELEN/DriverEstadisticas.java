260bec2e8f1d04dc625d96865096873e0e7098ab
belen95 <belen_95_22@hotmail.es>
2015-11-15 23:02:43 +0100

    he fusionado los drivers de estadisticas y ranking

--- FONTS/BELEN/DriverEstadisticas.java
+++ FONTS/BELEN/DriverEstadisticas.java
@@ -1,13 +1,14 @@
 package BELEN;
 
 import java.util.Scanner;
+import ALEX.SuperDriver;
 import JOEL.Jugador;
 
 public class DriverEstadisticas {
 
 	private Scanner scan;
 	private CtrlEstadisticas CE = new CtrlEstadisticas();
-
+	private CtrlRanking CR = new CtrlRanking();
 	
 	public DriverEstadisticas() {
 		scan = new Scanner(System.in);
@@ -20,26 +21,33 @@ public class DriverEstadisticas {
 		}
 		int ex = 1;
 		while (ex == 1) {
-			System.out.print("Consulta de estadisticas de usuario.\n");
+			System.out.print("Consulta de estadisticas de juego.\n");
 			System.out.print("1 - Consultar tus estadisticas.\n");
 			System.out.print("2 - Consultar las estadisticas de otro usuario.\n");
-			System.out.print("3 - Salir al menú principal.\n");
+			System.out.print("3 - Consultar el ranking de un tablero.\n");
+			System.out.print("4 - Salir al menú principal.\n");
 			int i = scan.nextInt();
 			scan.nextLine();
 			switch (i) {
 				case 1:
-					CE.mostrarEst(jActivo.consultar_nombre());
+					CE.mostrarEst(jActivo.consultar_Nombre());
 					break;
 				case 2:
 					System.out.print("Introduce un nombre de usuario:\n");
 					String jugador = scan.nextLine();
-
-
 					if (CE.existe()) CE.mostrarEst(jugador);
-
 					else System.out.print("El usuario no existe.\n");
 					break;
-				case 3: 
+				case 3:
+					System.out.print("Introduce un nombre de tablero.\n");
+					String tablero = scan.nextLine();
+					System.out.print("Introduce el numero de posiciones que quieres ver.\n");
+					int n = scan.nextInt();
+					scan.nextLine();
+					if (CR.cargarRanking(tablero)) CR.getTop(n);
+					else System.out.print("El tablero no existe.\n");
+					break;
+				case 4: 
 					ex = 0;
 					break;
 				default: 
