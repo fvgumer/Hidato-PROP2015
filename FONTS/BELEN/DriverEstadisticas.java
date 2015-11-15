c68b08bed0f68ff9524b6130719e9584232084a2
belen95 <belen_95_22@hotmail.es>
2015-11-15 21:39:33 +0100

    Update DriverEstadisticas.java

--- FONTS/BELEN/DriverEstadisticas.java
+++ FONTS/BELEN/DriverEstadisticas.java
@@ -1,25 +1,39 @@
 package BELEN;
 
-import java.util.*;
+import java.util.Scanner;
 
 public class DriverEstadisticas {
 
-	private static CtrlEstadisticas CE;
+	private Scanner scan;
+	private static CtrlEstadisticas CE = new CtrlEstadisticas();
 	
-	public static void main(String[] args) {
-		CE = new CtrlEstadisticas();
-		Scanner scan = new Scanner(System.in);
-		int i = 1;
-		while (i == 1) {
-			System.out.print("Introduce el nombre de un jugador para consultar sus estadisticas.\n");
-			String jugador = scan.nextLine();
-			boolean c = CE.cargarEst(jugador);
-			if (c) CE.mostrarEst();
-			System.out.print("Pulsa 1 para buscar otro usuario, -1 para salir.\n");
-			i = scan.nextInt();
-			scan.nextLine();
-		}
-		scan.close();
+	public DriverEstadisticas() {
+		scan = new Scanner(System.in);
 	}
+	
+	public void exec() {
+		int ex = 1;
+		while (ex == 1) {
+			System.out.print("Consulta de estadisticas de usuario.\n");
+			System.out.print("1 - Consultar las estadisticas de un usuario.\n");
+			System.out.print("2 - Salir al menú principal.\n");
+			int i = scan.nextInt();
+			scan.nextLine();
+			switch (i) {
+				case 1:
+					System.out.print("Introduce un nombre de usuario:\n");
+					String jugador = scan.nextLine();
+					if (CE.existeix()) CE.mostrarEst(jugador);
+					else System.out.print("El usuario no existe.\n");
+					break;
+				case 2: 
+					ex = 0;
+					break;
+				default: 
+					System.out.print("Introduce un codigo de operacion correcto.\n");
+					break;
+			}
+		}
+	}
+	
 }
-
