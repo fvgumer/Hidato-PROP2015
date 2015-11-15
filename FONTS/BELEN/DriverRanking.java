6e8c20f9ebb872b70cb1f8df2704a1665d011ede
belen95 <belen_95_22@hotmail.es>
2015-11-15 22:51:28 +0100

    holi

--- FONTS/BELEN/DriverRanking.java
+++ FONTS/BELEN/DriverRanking.java
@@ -1,12 +1,6 @@
 package BELEN;
 
 import java.util.Scanner;
-
-public class DriverRanking {
-
-	private static CtrlRankpackage BELEN;
-
-import java.util.Scanner;
 import JOEL.Jugador;
 
 public class DriverRanking {
@@ -45,24 +39,5 @@ public class DriverRanking {
 			}
 			
 		}
-	}
-}ing CR;
-	
-	public static void main(String[] args) {
-		CR = new CtrlRanking();
-		Scanner scan = new Scanner(System.in);
-		int i = 1;
-		while (i == 1) {
-			System.out.print("Introduce el nombre de un tablero y el numero de posiciones que quieres ver de su ranking.\n");
-			String tablero = scan.nextLine();
-			int n = scan.nextInt();
-			scan.nextLine();
-			int c = CR.cargarRanking(tablero);
-			if (c == 1)CR.getTop(n);
-			System.out.print("Pulsa 1 para consultar otro ranking.\nPulsa -1 para salir.\n");
-			i = scan.nextInt();
-			scan.nextLine();
-		}
-		scan.close();
 	}
 }
