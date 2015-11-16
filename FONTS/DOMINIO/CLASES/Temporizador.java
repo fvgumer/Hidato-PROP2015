1125419f8487c4e946b505ab9541594cbe8feba3
elena20ruiz <elena.ruiz.bdn@gmail.com>
2015-11-16 23:50:37 +0100

    Update Temporizador.java

--- FONTS/DOMINIO/CLASES/Temporizador.java
+++ FONTS/DOMINIO/CLASES/Temporizador.java
@@ -1,5 +1,4 @@
-package DOMINIO.CLASES;
-
+package ELENA;
 import javax.swing.Timer; 
 import java.awt.event.*; 
 import java.io.Serializable; 
@@ -10,124 +9,113 @@ public class Temporizador implements ActionListener,Serializable {
 	private static final long serialVersionUID = 1L;
 	Timer timer; 
 	private int min_max = 20;
-	private int segundos=0; 
-	private int minutos=0; 
+	private int segundos; 
+	private int minutos; 
 	private int modo; 
-	private boolean congelado=false;  
-	private boolean tacabado=false;
-	private boolean tapar = false;
+	private boolean congelado; 
+	private boolean acabar;
+	private boolean inicializar_tablero;
 	
 	ActionListener temps_maxim = new ActionListener() {
 	      public void actionPerformed(ActionEvent evt) {
 	    		  timer.stop();
 	      }
 	  };
+	  
+	  public Temporizador(){
+		  timer = new Timer(1000,this);
+	  }
+	  
+	  public void inicializar(int min, int modo) {
+		  timer = new Timer(1000,this);
+		  min_max = min;
+		  this.modo = modo;
+		  inicializar_tablero = false;
+		  acabar = false;
+		  congelado = false;
+		  minutos = 0;
+		  segundos = 0;
+		  if (modo == 1) estMinuto(min); 
+	  }
+		  
+		 public boolean inicializar_tablero() {
+			 return inicializar_tablero;
+		 }
+		 
+		 public boolean acabar() {
+			 return acabar;
+		 }
+		 
+		 public void setInicializar_tablero(boolean b) {
+			 inicializar_tablero=b;
+		 }
+		  
+		public void estMinuto(int min){ 
+			this.minutos=min; 
+		} 
 	
-	public void timer_max(){
-		timer = new Timer(20000,temps_maxim);
-	}
+		public void estSegundo(int seg){ 
+			this.segundos=seg; 
+		} 
 	
 	
-	public Temporizador(){ 
-	timer=new Timer(1000,this); 
-	} 
-
-	public Temporizador(int minuto,int segundo,int min_max, int modo){ 
-		timer=new Timer(1000,this); 
-		this.min_max = min_max;
-		estMinuto(minuto); 
-		estSegundo(segundo); 
-		this.modo = modo;
-		if (modo == 2) {
-			tapar = false;
+		public void iniciar() { 
+			if (!congelado)
+				timer.start();
 		}
-		else tapar = true;
-	
-	}
-	
-	public void estMinuto(int min){ 
-		this.minutos=min; 
-	} 
-
-	public void estSegundo(int seg){ 
-		this.segundos=seg; 
-	} 
-
-
-	public void iniciar() { 
-		if (!congelado)
+		
+		public boolean esta_congelado(){
+			return congelado;
+		}
+			
+		public boolean estaCorriendo(){ 
+			return timer.isRunning(); 
+		} 
+		
+		public void detenerse() { 
+			timer.stop(); 
+		} 
+		
+		public void reiniciar(){ 
 			timer.start();
-	}
-	
-	public boolean esta_congelado(){
-		return congelado;
-	}
 		
-	public boolean estaCorriendo(){ 
-		return timer.isRunning(); 
-	} 
-	
-	public void detenerse() { 
-		//Stop the animating thread. 
-		timer.stop(); 
-	} 
-	
-	public void reiniciar(){ 
-		timer.start();
-	
-	} 
-	public int obtMinuto(){ 
-		return this.minutos; 
-	} 
-	public int obtSegundo(){ 
-		return this.segundos; 
-	} 
-	
-	public boolean getTacabado(){
-		return tacabado;
-	}
-	
-	public boolean getTapar() {
-		return tapar;
-	}
-	
-	public void setTapar(int i) {
-		if (i == 0) tapar = false;
-		else tapar = true;
-	}
-	
-	public void actionPerformed2(ActionEvent e1) {
-		
-	}
-	
-	//Se Actualiza cada segundo
-	public void actionPerformed(ActionEvent e){
-		if(modo == 2) {
-			if (segundos == 0 && min_max == minutos){
-				tapar = true;
-			}
-			if (!congelado){
-				segundos++;
-				if (segundos > 60) {
-						minutos++; 
-						segundos=0;  
-				}  
-			}
+		} 
+		public int obtMinuto(){ 
+			return this.minutos; 
+		} 
+		public int obtSegundo(){ 
+			return this.segundos; 
 		}
-		else {
-			
-			if (!congelado) {
-				segundos++; 
-				if (segundos > 60) {
-						minutos++; 
-						segundos=0;  
-				}  
-				if (minutos == min_max && segundos == 0) {
-					tacabado = true;
-					congelado = true;
+
+		public void actionPerformed(ActionEvent e) {
+			if (modo == 0 || modo > 1) { //MODO CLASICO Y EXTREMO
+				if (!congelado){
+					segundos++;
+					if (segundos == 60) {
+							minutos++; 
+							segundos=0;  
+					}  
 				}
+				//MODO EXTREMO
+	    	  	if (modo == 2 && min_max == minutos && segundos ==0) {
+	    	  		inicializar_tablero = true;
+	    	  	}
+	    	  	if (modo == 3 && minutos == 0 && segundos == 30) {
+	    	  		timer.stop();
+	    	  	}
+			}
+			else {
+				if (!congelado){
+					segundos--;
+					if (segundos <= 0) {
+							minutos--; 
+							segundos=59;  
+					}  
+				}
+	    	  	if (minutos == 0 && segundos == 0) { //TIEMPO ACABADO
+	    	  		acabar = true;
+	    	  	}
 			}
 			
-		}
-	}
+		} 
 } 
