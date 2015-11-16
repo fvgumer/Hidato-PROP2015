f737fab53f96474160377b9c503a3d72352869e4
belen95 <belen_95_22@hotmail.es>
2015-11-16 02:02:04 +0100

    coment

--- FONTS/BELEN/ClassResultado.java
+++ FONTS/BELEN/ClassResultado.java
@@ -2,18 +2,36 @@ package BELEN;
 
 import java.io.Serializable;
 
-public class ClassResultado implements Serializable{
+/**
+ * Esta clase implementa las funciones consultoras y modificadoras del 
+ * resultado de una partida.
+ * @author Belen
+ *
+ */
 
-	/**
-	 * 
-	 */
-	private static final long serialVersionUID = 1L;
-	//private String tablero;
-	private String jugador;
-	private String modo;
-	private String dificultad;
-	private int puntuacion;
+public class ClassResultado implements Serializable {
 	
+	private String jugador;
+	//Nombre del usuario que ha jugado la partida.
+	
+	private String modo;
+	//Modo en que se ha jugado la partida.
+	
+	private String dificultad;
+	//Dificultad asignada a la partida.
+	
+	private int puntuacion;
+	//Puntuacion obtenida en la partida.
+
+	private static final long serialVersionUID = 1L;
+	
+	/**
+	 * Creadora por defecto de la clase.
+	 * @param jugador Jugador.
+	 * @param modo Modo de juego.
+	 * @param dificultad Dificultad asignada.
+	 * @param puntuacion Puntuacion obtenida.
+	 */
 	public ClassResultado(String jugador, String modo, String dificultad, int puntuacion) {
 		this.jugador = jugador;
 		this.modo = modo;
@@ -21,14 +39,25 @@ public class ClassResultado implements Serializable{
 		this.puntuacion = puntuacion;
 	}
 	
+	/**
+	 * Consultora del jugador asociado al resultado.
+	 * @return Nombre de dicho jugador.
+	 */
 	public String getJugador() {
 		return jugador;
 	}
 	
+	/**
+	 * Consultora de la puntuacion obtenida en la partida.
+	 * @return Dicha puntuacion.
+	 */
 	public int getPuntuacion() {
 		return puntuacion;
 	}
 	
+	/**
+	 * Metodo que muestra por pantalla el resultado.
+	 */
 	public void mostrarResultado(){
 		System.out.format("%s  ||  %s ||  %s  ||  %d\n",jugador,modo,dificultad,puntuacion);
 	} 
