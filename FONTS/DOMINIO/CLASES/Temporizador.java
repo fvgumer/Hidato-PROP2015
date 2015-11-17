e8fd2c5cc4957fc1b8a7a6afab735b3a54b6ec75
elena20ruiz <elena.ruiz.bdn@gmail.com>
2015-11-17 07:22:06 +0100

    Update Temporizador.java

--- FONTS/DOMINIO/CLASES/Temporizador.java
+++ FONTS/DOMINIO/CLASES/Temporizador.java
@@ -1,8 +1,7 @@
-package ELENA;
+package DOMINIO.CLASES.*;
 import javax.swing.Timer; 
 import java.awt.event.*; 
 import java.io.Serializable; 
-import java.awt.Color; 
 
 public class Temporizador implements ActionListener,Serializable { 
 
@@ -21,6 +20,10 @@ public class Temporizador implements ActionListener,Serializable {
 	    		  timer.stop();
 	      }
 	  };
+	  
+	  public void temps_maxim(){
+		  timer = new Timer(30000,temps_maxim);
+	  }
 	  
 	  public Temporizador(){
 		  timer = new Timer(1000,this);
@@ -49,44 +52,82 @@ public class Temporizador implements ActionListener,Serializable {
 		 public void setInicializar_tablero(boolean b) {
 			 inicializar_tablero=b;
 		 }
-		  
+		 /**
+		 * Introduce los minutos iniciales
+		 * @param seg entero que hace referencia a los minutos
+		 * en que queremos que comienze el timer
+		 */
 		public void estMinuto(int min){ 
 			this.minutos=min; 
 		} 
-	
+		/**
+		 * Introduce los segundos iniciales
+		 * @param seg entero que hace referencia a los segundos
+		 * en que queremos que comienze el timer
+		 */
 		public void estSegundo(int seg){ 
 			this.segundos=seg; 
 		} 
 	
-	
+		/**
+		 * Inicia el contador del timer
+		 */
 		public void iniciar() { 
 			if (!congelado)
 				timer.start();
 		}
-		
+		/**
+		 * Consulta del parametro congelado
+		 * @return Sera cierto si el timer esta parado,
+		 * sera falso si el timer esta ejecutandose
+		 */
 		public boolean esta_congelado(){
 			return congelado;
 		}
-			
+		/**
+		 * Consulta del estado del timer
+		 * @return Devuelve cierto
+		 * si el timer se esta ejecutando, devuelve
+		 * falso si esta parado.
+		 */
 		public boolean estaCorriendo(){ 
 			return timer.isRunning(); 
 		} 
-		
+		/**
+		 * Para el timer
+		 */
 		public void detenerse() { 
 			timer.stop(); 
 		} 
-		
+		/**
+		 * Reinicia el timer
+		 */
 		public void reiniciar(){ 
 			timer.start();
 		
 		} 
+		/**Consula de los minutos
+		 * @return Devuelve un entero que corresponde
+		 * a los minutos que han pasado desde que el 
+		 * timer ha comenzado
+		 */
 		public int obtMinuto(){ 
 			return this.minutos; 
 		} 
+		/**Consula de los segundo
+		 * @return Devuelve un entero que corresponde
+		 * a los segundos que han pasado desde que el 
+		 * timer ha comenzado
+		 */
 		public int obtSegundo(){ 
 			return this.segundos; 
 		}
-
+		/**
+		 * Funcion del timer.
+		 * Lleva a cabo un ActionEvent del timer
+		 * que hace varias funciones segun el modo de juego,
+		 * principalmente contar o decrementar los segundos
+		 */
 		public void actionPerformed(ActionEvent e) {
 			if (modo == 0 || modo > 1) { //MODO CLASICO Y EXTREMO
 				if (!congelado){
@@ -94,15 +135,16 @@ public class Temporizador implements ActionListener,Serializable {
 					if (segundos == 60) {
 							minutos++; 
 							segundos=0;  
-					}  
+					}
+					//MODO EXTREMO
+		    	  	if (modo == 2 && min_max == minutos && segundos ==0) {
+		    	  		inicializar_tablero = true;
+		    	  	}
+		    	  	if (modo == 3 && minutos == 0 && segundos == 30) {
+		    	  		timer.stop();
+		    	  	}
 				}
-				//MODO EXTREMO
-	    	  	if (modo == 2 && min_max == minutos && segundos ==0) {
-	    	  		inicializar_tablero = true;
-	    	  	}
-	    	  	if (modo == 3 && minutos == 0 && segundos == 30) {
-	    	  		timer.stop();
-	    	  	}
+				
 			}
 			else {
 				if (!congelado){
@@ -110,11 +152,11 @@ public class Temporizador implements ActionListener,Serializable {
 					if (segundos <= 0) {
 							minutos--; 
 							segundos=59;  
-					}  
+					}
+		    	  	if (minutos == 0 && segundos == 0) { //TIEMPO ACABADO
+		    	  		acabar = true;
+		    	  	}
 				}
-	    	  	if (minutos == 0 && segundos == 0) { //TIEMPO ACABADO
-	    	  		acabar = true;
-	    	  	}
 			}
 			
 		} 
