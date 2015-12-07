8854864a948e2b4018f63b18368147d4fad9638a
Elena_Ruiz <elena.ruiz.bdn@gmail.com>
2015-12-07 19:04:03 +0100

    Pujo mes coses

---
+++ FONTS/VISTAS/Titulo.java
@@ -0,0 +1,21 @@
+package VISTAS;
+
+
+import java.awt.Font;
+import javax.swing.JTextPane;
+
+import java.awt.Color;
+
+public class Titulo extends JTextPane {
+	private static final long serialVersionUID = 1L;
+
+
+		public Titulo (String texto, int x, int y) {
+			this.setForeground(new Color(255, 250, 240));
+			this.setBounds(x, 53, y, 50);
+			this.setEditable(false);
+			this.setFont(new Font("Graphite Std Light", Font.BOLD, 37));
+			this.setText(texto);
+			this.setBackground(null);
+		}
+}
--- FONTS/VISTAS/VistaInicial.java
+++ FONTS/VISTAS/VistaInicial.java
@@ -1,46 +1,15 @@
 package VISTAS;
 
-import java.awt.BorderLayout;
-import java.awt.EventQueue;
-import java.awt.Font;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
 
-import javax.swing.JButton;
-import javax.swing.JFrame;
-import javax.swing.JPanel;
-import javax.swing.JTextPane;
-import javax.swing.border.EmptyBorder;
-import javax.swing.JLabel;
-import com.jgoodies.forms.factories.DefaultComponentFactory;
-import java.awt.Color;
-import java.awt.event.MouseAdapter;
-import java.awt.event.MouseEvent;
-import javax.swing.JMenuBar;
+import java.awt.EventQueue;
 import java.awt.Button;
 import java.awt.SystemColor;
 
 public class VistaInicial extends VistaPadreInicio {
 
-	private static final JTextPane JTextPane = null;
 	private Button b1;
 	private Button b2;
 
-
-	public void anadir_titulo(String texto,int x, int y) {
-		JTextPane txtpnJuegoHidato = new JTextPane();
-		txtpnJuegoHidato.setForeground(new Color(255, 250, 240));
-		txtpnJuegoHidato.setBounds(x, 56, y, 50);
-		txtpnJuegoHidato.setEditable(false);
-		txtpnJuegoHidato.setFont(new Font("Graphite Std Light", Font.BOLD, 37));
-		txtpnJuegoHidato.setText(texto);
-		txtpnJuegoHidato.setBackground(null);
-		contentPane.add(txtpnJuegoHidato);
-	}
-	
-	public void anadir_boton(Button b, int x, int y){
-		
-	}
 
 	public void main(String[] args) {
 		EventQueue.invokeLater(new Runnable() {
@@ -60,7 +29,8 @@ public class VistaInicial extends VistaPadreInicio {
 		//Config layer 
 		super.setTextLayer("Inicio");
 		contentPane.setLayout(null);
-		anadir_titulo("JUEGO HIDATO",130,247);
+		Titulo t = new Titulo("JUEGO HIDATO",130,247);
+		contentPane.add(t);
 		
 		
 		Botones b1 = new Botones("Entrar",50,200);
@@ -68,11 +38,6 @@ public class VistaInicial extends VistaPadreInicio {
 		Botones b2 = new Botones("Salir",250,200);
 		contentPane.add(b2);		
 		
-		Button button = new Button("Hola\r\n");
-		button.setForeground(SystemColor.text);
-		button.setBackground(SystemColor.desktop);
-		button.setBounds(0, 0, 124, 62);
-		contentPane.add(button);
 
 	}
 }
