72399728c40a6ab35868e2f291eb9440871a8774
fvgumer <joel.codina@est.fib.upc.edu>
2015-12-08 18:36:00 +0100

    hi

--- FONTS/CLUSTER/VISTAS/VistaConsultaEst.java
+++ FONTS/CLUSTER/VISTAS/VistaConsultaEst.java
@@ -6,6 +6,8 @@ import javax.swing.JFrame;
 import javax.swing.JTextField;
 import javax.swing.GroupLayout;
 import javax.swing.GroupLayout.Alignment;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
 
 public class VistaConsultaEst extends VistaPadreInicio{
 
@@ -36,15 +38,31 @@ public class VistaConsultaEst extends VistaPadreInicio{
 		super.setTextLayer("Consulta Estadísticas");
 		getContentPane().setName("Consulta Estadísticas");
 		Botones B1 = new Botones("Estadísticas personales", 100, 60);
+		B1.addMouseListener(new MouseAdapter() {
+			public void mouseClicked(MouseEvent arg0) {
+			}
+		});
 		B1.setSize(272, 42);
 		getContentPane().add(B1);
 		Botones B2 = new Botones("Estadísticas de usuario", 100, 130);
+		B2.addMouseListener(new MouseAdapter() {
+			public void mouseClicked(MouseEvent e) {
+			}
+		});
 		B2.setSize(272, 42);
 		getContentPane().add(B2);
 		Botones B3 = new Botones("Ranking de tablero", 100, 200);
+		B3.addMouseListener(new MouseAdapter() {
+			public void mouseClicked(MouseEvent e) {
+			}
+		});
 		B3.setSize(272, 42);
 		getContentPane().add(B3);
 		Botons Batras = new Botons("Atrás");
+		Batras.addMouseListener(new MouseAdapter() {
+			public void mouseClicked(MouseEvent e) {
+			}
+		});
 		getContentPane().add(Batras);
 	}
 
