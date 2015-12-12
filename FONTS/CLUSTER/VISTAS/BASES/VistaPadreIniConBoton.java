1da5f423bfe8ee6a4c3120b153b75e848596a444
Elena_Ruiz <elena.ruiz.bdn@gmail.com>
2015-12-12 12:10:15 +0100

    mc

--- FONTS/CLUSTER/VISTAS/BASES/VistaPadreIniConBoton.java
+++ FONTS/CLUSTER/VISTAS/BASES/VistaPadreIniConBoton.java
@@ -21,15 +21,10 @@ public class VistaPadreIniConBoton extends VistaPadreInicio {
 		JB.setBounds(617, 396, 87, 47);
 		getContentPane().add(JB);
 		JB.setIcon(new ImageIcon(BotonAtras.class.getResource("/com/sun/javafx/scene/control/skin/caspian/fxvk-backspace-button.png")));
-		//setIcon(new ImageIcon("../IMG/4906.png"));
 		JB.setBackground(null);
 		JB.setForeground(null);
 		JB.setBorder(null);
-<<<<<<< HEAD
-		JB.setBounds(370, 240, 87, 47);
-		getContentPane().add(JB);
-=======
->>>>>>> origin/master
+		//JB.setBounds(370, 240, 87, 47);
 	}
 
 }
