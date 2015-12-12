bdf4ee33e284be9c24e665c3ca9c36f429fa9495
belen95 <belen_95_22@hotmail.es>
2015-12-12 21:21:54 +0100

    g

--- FONTS/CLUSTER/DOMINIO/CONTROLADORES/CtrlDominio.java
+++ FONTS/CLUSTER/DOMINIO/CONTROLADORES/CtrlDominio.java
@@ -52,7 +52,6 @@ public class CtrlDominio {
 		return CTablero.get_tablero();
 	}
 
-<<<<<<< HEAD
 	public void guardar_tablero() {
 		CTablero.guardar();
 	}
@@ -60,7 +59,6 @@ public class CtrlDominio {
 	public void eliminar_tablero() {
 		CTablero.eliminar();
 	}
-=======
 	public boolean crearUsuario(String nombre, String password) {
 		return CJugador.crear_usuario(nombre, password);
 	}
@@ -70,5 +68,4 @@ public class CtrlDominio {
 		CRanking.getTop(nTab,nPos);
 	}
 
->>>>>>> 9a1a44810b06864fd68eba1e6346c126d8d86154
 }
