927dc17d582b01b71e5ded1d3d6d38e45df82456
fvgumer <joel.codina@est.fib.upc.edu>
2015-12-12 18:42:39 +0100

    jo

--- FONTS/CLUSTER/VISTAS/ESTADISTICAS/VistaConsultaEst.java
+++ FONTS/CLUSTER/VISTAS/ESTADISTICAS/VistaConsultaEst.java
@@ -38,37 +38,40 @@ public class VistaConsultaEst extends VistaPadreIniConBoton{
 	 */
 	public VistaConsultaEst(final CtrlVista CV) {
 		
-		super.setTextLayer("Consulta Estadísticas");
-		getContentPane().setName("Consulta Estadísticas");
+		super.setTextLayer("Consulta Estadisticas");
+		getContentPane().setName("Consulta Estadisticas");
 		
-		Botones B1 = new Botones("Estadísticas personales", 100, 60);
+		Botones B1 = new Botones("Estadisticas personales", 100, 60);
+		B1.setLocation(130, 83);
 		B1.addMouseListener(new MouseAdapter() {
 			public void mouseClicked(MouseEvent arg0) {
 				CV.entrarAEstPersonales();
 				Salir();
 			}
 		});
-		B1.setSize(272, 42);
+		B1.setSize(448, 42);
 		getContentPane().add(B1);
 		
-		Botones B2 = new Botones("Estadísticas de usuario", 100, 130);
+		Botones B2 = new Botones("Estadisticas de usuario", 100, 130);
+		B2.setLocation(130, 167);
 		B2.addMouseListener(new MouseAdapter() {
 			public void mouseClicked(MouseEvent e) {
 				CV.entrarAEstUsuario();
 				Salir();
 			}
 		});
-		B2.setSize(272, 42);
+		B2.setSize(448, 42);
 		getContentPane().add(B2);
 		
 		Botones B3 = new Botones("Ranking de tablero", 100, 200);
+		B3.setLocation(130, 251);
 		B3.addMouseListener(new MouseAdapter() {
 			public void mouseClicked(MouseEvent e) {
 				CV.entrarARanking();
 				Salir();
 			}
 		});
-		B3.setSize(272, 42);
+		B3.setSize(448, 42);
 		getContentPane().add(B3);
 		
 		/*super.addMouseListener(new MouseAdapter() {
