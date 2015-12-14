4a89fdd3d66a221979e344584e0e47db76642c66
fvgumer <fvgumer@users.noreply.github.com>
2015-12-14 14:02:44 +0100

    Update CtrlDominio.java

--- FONTS/CLUSTER/DOMINIO/CONTROLADORES/CtrlDominio.java
+++ FONTS/CLUSTER/DOMINIO/CONTROLADORES/CtrlDominio.java
@@ -13,7 +13,7 @@ public class CtrlDominio {
 	private CtrlPartida CPartida;
 	private CtrlRanking CRanking;
 	private CtrlTablero CTablero;
-	private Jugador Jactivo = new Jugador(null,null);
+	private Jugador Jactivo = null;
 	
 	public CtrlDominio() {
 		CEstadisticas = new CtrlEstadisticas();
