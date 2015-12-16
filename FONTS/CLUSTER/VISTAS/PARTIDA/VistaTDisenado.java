97a6acdbd8984f953797a3cba4c7a63d2d52b684
AlexCatala <alex.catala.cabello@gmail.com>
2015-12-16 16:05:21 +0100

    tdis

--- FONTS/CLUSTER/VISTAS/PARTIDA/VistaTDisenado.java
+++ FONTS/CLUSTER/VISTAS/PARTIDA/VistaTDisenado.java
@@ -2,37 +2,55 @@ package CLUSTER.VISTAS.PARTIDA;
 
 import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
+
+import javax.swing.DefaultListModel;
+import javax.swing.JList;
+
 import CLUSTER.VISTAS.CONTROLADORES.CtrlVista;
 
 
 public class VistaTDisenado extends VistaPrevisualizarTableroPadre {
 
+	private DefaultListModel listModel;
+	private CtrlVista CV;
+	private JList list;
+	
 	public VistaTDisenado(final CtrlVista CV) {
 		super(CV);
+		this.CV = CV;
 		txt = "Elegir Tablero Diseñado";
-		
-		list.addMouseListener(new MouseAdapter() {
-			public void mouseClicked(MouseEvent arg0) {
-					//FUNCION
-				CV.listarTableros();
-			}
-		});
-
-		Siguiente.addMouseListener(new MouseAdapter() {
-			public void mouseClicked(MouseEvent arg0) {
-				/*if (!list.isSelectionEmpty()) 
-					//FUNCION
-				else {
-					//FUNCION
-				}*/
-			}
-		});
 		
 		JB.addMouseListener(new MouseAdapter() {
 			public void mouseClicked(MouseEvent arg0) {
 				CV.entrarAElegirForma();
 			}
 		});	
+	}
+	
+	public void actualitza_llista() {
+		listModel = new DefaultListModel();
+		String[] s = CV.get_tableros_repo();
+		for(int i=0; i < s.length; ++i) {
+			if (!s[i].contains(".txt")) {
+				listModel.addElement(s[i]);
+			}
+		}
+		list = new JList(listModel);
+		getContentPane().add(list);
+		
+		list.setBounds(349,42,239,392);
+		list.addMouseListener(new MouseAdapter() {
+			@Override
+			public void mouseClicked(MouseEvent e) {
+				if (list.getSelectedValue() != null) {
+					String id = "";
+					id = list.getSelectedValue().toString().substring(0, 8);
+					CV.entrarABorrarConfirmar(id);
+					Salir();
+				}
+			}
+		});
+
 	}
 
 }
